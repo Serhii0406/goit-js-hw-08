ad6de3b9e6547a4be6f38bc2c324cbb114b8dfa0
Serhii0406 <106251203+Serhii0406@users.noreply.github.com>
2022-10-20 14:04:04 +0300

    commit
    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryDiv = document.querySelector('.gallery');
+const galleryMakrUp = createGallary(galleryItems);
 
-console.log(galleryItems);
+galleryDiv.insertAdjacentHTML('beforeend', galleryMakrUp);
+
+function createGallary (galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</div>`})
+    .join(' ');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
