1b8368ade91207b804d63c7dd10085a6c31a7879
Serhii0406 <106251203+Serhii0406@users.noreply.github.com>
2022-10-22 13:07:13 +0300

    commit
    task 1 commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,10 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 const galleryDiv = document.querySelector('.gallery');
 const galleryMakrUp = createGallary(galleryItems);
 
